a03a75014f2acde456adcb010203d1b85af81362
Evandro Theodoro <evandrolt@gmail.com>
2023-05-10 22:45:38 -0300

    Configurando o decorator @ApiProperty nos DTO's

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreatePostDto {
+
+  @ApiProperty({description: 'Título do post'})
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({description: 'Conteúdo do post'})
   @IsString()
   @IsOptional()
   content: string;
 
+  @ApiProperty({description: 'Email do autor do post'})
   @IsEmail()
   authorEmail: string;
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsBoolean, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
 
+  @ApiProperty({description: 'Email do usuário'})
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({description: 'Nome completo do usuário'})
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Define se o usuário é administrador',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 
