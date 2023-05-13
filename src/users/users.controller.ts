28b7f6bc59a76846e36deb24b7291839767b2ac1
Evandro Theodoro <evandrolt@gmail.com>
2023-05-13 20:16:49 -0300

    Usando Tags para agrupar os endpoints na documentação

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
+import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -15,21 +16,24 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiForbiddenResponse({description: 'Acesso Negado'})
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiForbiddenResponse({description: 'Acesso Negado'})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
-
+  @ApiForbiddenResponse({description: 'Acesso Negado'})
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @ApiForbiddenResponse({description: 'Acesso Negado'})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
