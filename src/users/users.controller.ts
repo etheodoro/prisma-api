c84575720bf588a97cd160d4f3c37145d5bb29b3
Evandro Theodoro <evandrolt@gmail.com>
2023-05-13 20:07:14 -0300

    Ajustando as API Responses na documentação

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiResponse({status: 409, description: 'Conflito de email'})
+  @ApiForbiddenResponse({description: 'Acesso Negado'})
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
